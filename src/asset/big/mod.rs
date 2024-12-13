56b758e3ae4e175703aa0ca8b97cac1ff7010240
Nguyen_Do_IoT <do30032003@gmail.com>
2024-12-13 09:59:41 +0000

    up ble

---
+++ src/asset/big/mod.rs
@@ -0,0 +1,1 @@
+pub mod bien_90;
---
+++ src/main.rs
@@ -0,0 +1,362 @@
+#![no_std]
+#![no_main]
+use bleps::{
+    ad_structure::{
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
+    },
+    attribute_server::{AttributeServer, NotificationData, WorkResult},
+    gatt, Ble, HciConnector,
+};
+use display_interface_spi::SPIInterface;
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    mono_font::{
+        ascii::{FONT_10X20, FONT_7X13},
+        MonoTextStyle,
+    },
+    pixelcolor::{raw::BigEndian, Rgb565},
+    prelude::*,
+    primitives::{Arc, Circle, PrimitiveStyleBuilder},
+    text::Text,
+};
+use embedded_hal_bus::spi::ExclusiveDevice;
+use esp_alloc as _;
+use esp_alloc as _;
+use esp_backtrace as _;
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{self, Input, Io, Level, NoPin, Output, Pull},
+    prelude::*,
+    rng::Rng,
+    spi::{master::Spi, SpiMode},
+    time,
+    timer::timg::TimerGroup,
+};
+use esp_println::println;
+use esp_wifi::ble::controller::BleConnector;
+use esp_wifi::EspWifiInitFor;
+use mipidsi::{
+    models::GC9A01,
+    options::{ColorInversion, ColorOrder},
+    Builder,
+};
+pub mod asset;
+
+fn get_cell_position(row: usize, col: usize, cell_size: usize) -> Point {
+    let x = (col * cell_size) as i32;
+    let y = (row * cell_size) as i32;
+    Point::new(x, y)
+}
+#[entry]
+fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+    let config = esp_hal::Config::default();
+    let peripherals = esp_hal::init(config);
+    esp_alloc::heap_allocator!(72 * 1024);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let init = esp_wifi::init(
+        EspWifiInitFor::Ble,
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let button = Input::new(io.pins.gpio0, Pull::Down);
+    let mut debounce_cnt = 500;
+    let mut delay = Delay::new();
+
+    // LCD Code
+
+    let dc = Output::new(io.pins.gpio27, Level::High);
+    let sck = io.pins.gpio14;
+    let miso = io.pins.gpio12;
+    let mosi = io.pins.gpio15;
+    let cs = io.pins.gpio5;
+    let spi = Spi::new(peripherals.SPI2, 40u32.MHz(), SpiMode::Mode0).with_pins(
+        sck,
+        mosi,
+        miso,
+        gpio::NoPin,
+    );
+    let cs_output = Output::new(cs, Level::High);
+    let mut rst = Output::new(io.pins.gpio33, Level::Low);
+    rst.set_high();
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs_output).unwrap();
+    let di = SPIInterface::new(spi_device, dc);
+    let mut display: mipidsi::Display<
+        SPIInterface<ExclusiveDevice<_, Output<'_>, embedded_hal_bus::spi::NoDelay>, Output<'_>>,
+        GC9A01,
+        Output<'_>,
+    > = Builder::new(GC9A01, di)
+        .invert_colors(ColorInversion::Normal)
+        .display_size(240, 240)
+        .color_order(ColorOrder::Bgr)
+        .invert_colors(ColorInversion::Inverted)
+        .reset_pin(rst)
+        .init(&mut delay)
+        .unwrap();
+    display.clear(Rgb565::new(5, 12, 8)).unwrap();
+    let cell_size = 10;
+    let rows = 240 / cell_size;
+    let cols = 240 / cell_size;
+    let circle = Circle::new(Point::zero(), 240).into_styled(
+        PrimitiveStyleBuilder::new()
+            .stroke_color(Rgb565::new(5, 12, 8))
+            .build(),
+    );
+    circle.draw(&mut display).unwrap();
+
+    let row = 15;
+    let col = 8;
+    let position = get_cell_position(row, col, cell_size);
+    let raw_image = ImageRaw::<Rgb565, BigEndian>::new(crate::asset::big::bien_90::test::DATA, 80);
+    let image = Image::new(&raw_image, position);
+    image.draw(&mut display).unwrap();
+
+
+    let row = 6;
+    let col = 6;
+    let position = get_cell_position(row, col, cell_size);
+    let raw_image = ImageRaw::<Rgb565, BigEndian>::new(crate::asset::small::bien_90::test::DATA, 50);
+    let image = Image::new(&raw_image, position);
+    image.draw(&mut display).unwrap();
+
+    let row = 6;
+    let col = 13;
+    let position = get_cell_position(row, col, cell_size);
+    let raw_image = ImageRaw::<Rgb565, BigEndian>::new(crate::asset::small::bien_90::test::DATA, 50);
+    let image = Image::new(&raw_image, position);
+    image.draw(&mut display).unwrap();
+
+    Text::new(
+        "17:30",
+        Point::new(95, 118),
+        MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "10",
+        Point::new(20, 180),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "20",
+        Point::new(7, 153),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "30",
+        Point::new(1, 118),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "40",
+        Point::new(7, 85),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "50",
+        Point::new(28, 50),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "60",
+        Point::new(66, 22),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "70",
+        Point::new(112, 13),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "80",
+        Point::new(167, 22),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "90",
+        Point::new(200, 50),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "100",
+        Point::new(213, 85),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "110",
+        Point::new(220, 118),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "120",
+        Point::new(215, 153),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    Text::new(
+        "130",
+        Point::new(205, 180),
+        MonoTextStyle::new(&FONT_7X13, Rgb565::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+    let mut i: f32 = 0.0;
+
+    // BLE
+    let mut bluetooth = peripherals.BT;
+    let now = || time::now().duration_since_epoch().to_millis();
+    loop {
+        // BLE
+        let connector = BleConnector::new(&init, &mut bluetooth);
+        let hci = HciConnector::new(connector, now);
+        let mut ble = Ble::new(&hci);
+        println!("{:?}", ble.init());
+        println!("{:?}", ble.cmd_set_le_advertising_parameters());
+        println!(
+            "{:?}",
+            ble.cmd_set_le_advertising_data(
+                create_advertising_data(&[
+                    AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+                    AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
+                    AdStructure::CompleteLocalName("ducanhkhuong-ble"),
+                ])
+                .unwrap()
+            )
+        );
+        println!("{:?}", ble.cmd_set_le_advertise_enable(true));
+        println!("started advertising");
+        let mut rf = |_offset: usize, data: &mut [u8]| {
+            data[..20].copy_from_slice(&b"Hello Bare-Metal BLE"[..]);
+            17
+        };
+        let mut wf = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, unsafe {
+                core::str::from_utf8_unchecked(data)
+            });
+        };
+        let mut wf2 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, unsafe {
+                core::str::from_utf8_unchecked(data)
+            });
+        };
+        let mut rf3 = |_offset: usize, data: &mut [u8]| {
+            data[..5].copy_from_slice(&b"Hola!"[..]);
+            5
+        };
+        let mut wf3 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: Offset {}, data {:?}", offset, data);
+        };
+        gatt!([service {
+            uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+            characteristics: [
+                characteristic {
+                    uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+                    read: rf,
+                    write: wf,
+                },
+                characteristic {
+                    uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
+                    write: wf2,
+                },
+                characteristic {
+                    name: "my_characteristic",
+                    uuid: "987312e0-2354-11eb-9f10-fbc30a62cf38",
+                    notify: true,
+                    read: rf3,
+                    write: wf3,
+                },
+            ],
+        },]);
+        let mut rng = bleps::no_rng::NoRng;
+        let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
+        loop {
+            // LCD logic
+            let arc = Arc::new(Point::new(32, 30), 174, 135.0.deg(), i.deg()).into_styled(
+                PrimitiveStyleBuilder::new()
+                    .stroke_color(Rgb565::new(30, 15, 11))
+                    .stroke_width(17)
+                    .build(),
+            );
+            arc.draw(&mut display).unwrap();
+            i += 5.0;
+            delay.delay_millis(200);
+            if i == 270.0 {
+                i = 0.0;
+            } else {
+            }
+            let arc = Arc::new(
+                Point::new(32, 30),
+                174,
+                (135.0 + i).deg(),
+                270.0.deg() - i.deg(),
+            )
+            .into_styled(
+                PrimitiveStyleBuilder::new()
+                    .stroke_color(Rgb565::CSS_GRAY)
+                    .stroke_width(17)
+                    .build(),
+            );
+            arc.draw(&mut display).unwrap();
+            let mut notification = None;
+            if button.is_low() && debounce_cnt > 0 {
+                debounce_cnt -= 1;
+                if debounce_cnt == 0 {
+                    let mut cccd = [0u8; 1];
+                    if let Some(1) = srv.get_characteristic_value(
+                        my_characteristic_notify_enable_handle,
+                        0,
+                        &mut cccd,
+                    ) {
+                        if cccd[0] == 1 {
+                            notification = Some(NotificationData::new(
+                                my_characteristic_handle,
+                                &b"Notification"[..],
+                            ));
+                        }
+                    }
+                }
+            };
+            if button.is_high() {
+                debounce_cnt = 500;
+            }
+            match srv.do_work_with_notification(notification) {
+                Ok(res) => {
+                    if let WorkResult::GotDisconnected = res {
+                        break;
+                    }
+                }
+                Err(err) => {
+                    println!("{:?}", err);
+                }
+            }
+        }
+    }
+}
